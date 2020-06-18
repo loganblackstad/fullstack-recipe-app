217c3fee9d9a152822542b603fd2ec46a83d82ba
Logan Blackstad <logan.blackstad@gmail.com>
2020-06-18 16:05:11 -0400

    more api requests

--- routes/api.js
+++ routes/api.js
@@ -34,6 +34,40 @@ router.get('/recipes/:id', function (req, res) {
     })
 });
 
+router.post('/recipes', (req, res) => {
+  // destructure the body
+  const { name, review, description, url, likes, vegetarian, vegan, glutenfree, categories } = req.body
 
+  // check for name, review, and url
+  if (!name) { res.status(400).json({ error: 'name field is required' }) }
+  if (!review) { res.status(400).json({ error: 'review field is required' }) }
+  if (!url) { res.status(400).json({ error: 'url field is required' }) }
+
+  // creating a new recipe for all fields (using defaults too)
+  db.Recipes.create({
+    name: name,
+    review: review,
+    description: description || '',
+    url: url,
+    likes: likes || 0,
+    vegetarian: vegetarian || false,
+    vegan: vegan || false,
+    glutenfree: glutenfree || false,
+  })
+    .then(recipe => {
+      return recipe.addCategories(categories)
+        .then(categories => {
+          res.status(201).json(recipe);
+        })
+    })
+    .catch(error => {
+      if (error.name === 'SequelizeForeignKeyConstraintError') {
+        res.json({ error: 'could not find all categories' })
+      } else {
+        res.json({ error: 'Server Error' });
+        console.log(error);
+      }
+    })
+});
 
 module.exports = router;
