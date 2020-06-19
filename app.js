06d6c752e29b18278cdf1a4a2471c3bbcb285613
Logan Blackstad <logan.blackstad@gmail.com>
2020-06-19 13:46:20 -0400

    added a react client

--- app.js
+++ app.js
@@ -14,15 +14,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // TODO: put react app here
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 // app.use('/users', usersRouter);
 
 app.use('/api/v1', apiRouter);
 
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+})
+
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
 app.use(function (err, req, res, next) {
