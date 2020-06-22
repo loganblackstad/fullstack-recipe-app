fd3ab8d561fa0cc3de7e72bb154a6c345e480bb1
Logan Blackstad <logan.blackstad@gmail.com>
2020-06-22 10:54:41 -0400

    added a funtional likes button

--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
-
+import './Recipes.css';
 
 export default class Recipes extends Component {
   constructor(props) {
@@ -21,19 +21,36 @@ export default class Recipes extends Component {
       })
   }
 
+  addLike = (index) => {
+    // create a recplica of the new state
+    var recipeGettingLiked = { ...this.state.recipes[index] };
+    // find the recipe with the given id and create a copy of that
+    recipeGettingLiked.likes = recipeGettingLiked.likes + 1;
+    // update the likes on that copy
+    // insert the newRecipes and setState
+    var newRecipes = this.state.recipes.map((recipe, ndx) => {
+      if (ndx === index) {
+        return recipeGettingLiked;
+      }
+      return recipe;
+    });
+    this.setState({ recipes: newRecipes })
+  };
+
   render() {
     return (
       <div>
-        {this.state.recipes.map(recipe => {
+        {this.state.recipes.map((recipe, index) => {
           return (
             <div key={recipe.id}>
-              <h1>{recipe.name}</h1>
+              <h1>{recipe.name}<button className="btn-delete">Delete</button></h1>
               <p>Review: {recipe.review}</p>
               <p>Description: {recipe.description}</p>
-              <p>Likes: {recipe.likes}</p>
+              <p>Likes: {recipe.likes} </p>
+              <button type="button" onClick={() => { this.addLike(index) }}>Add Like</button>
               <p>ID: {recipe.id}</p>
 
-              <NavLink to={`/recipes/${recipe.id}`}>Show Details</NavLink>
+              <Link to={`/recipes/${recipe.id}`}>Show Details</Link>
             </div>
           )
         })}
